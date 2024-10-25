76db842cbb4e2c56c131abd7ca0a42be11d69af8
kostya <reptilmo@gmail.com>
2024-10-25 00:52:48 -0700

    Draw a triangle.

--- source/main.cpp
+++ source/main.cpp
@@ -2,19 +2,7 @@
 
 #include "logging.h"
 #include "system.h"
-
-bool visuals_startup() {
-  glClearColor(0.5f, 0.3f, 0.3f, 1.0f);
-  return true;
-}
-
-void visuals_run() {
-  glClear(GL_COLOR_BUFFER_BIT);
-}
-
-void visuals_shutdown() {
-
-}
+#include "themepark.h"
 
 int main(int argc, char* argv[]) {
   Themepark::SystemContext context = {0};
@@ -22,9 +10,9 @@ int main(int argc, char* argv[]) {
   context.width = 640;
   context.height = 480;
   context.fullscreen = false;
-  context.client_startup = visuals_startup;
-  context.client_run = visuals_run;
-  context.client_shutdown = visuals_shutdown;
+  context.client_startup = Themepark::themepark_startup;
+  context.client_run = Themepark::themepark_run;
+  context.client_shutdown = Themepark::themepark_shutdown;
 
   if (Themepark::system_startup(&context)) {
     Themepark::system_run(&context);
--- source/themepark.cpp
+++ source/themepark.cpp
@@ -3,15 +3,74 @@
 // CS447P
 // Themepark
 
+#include "defines.h"
+
+#include <glad/glad.h>
+
 namespace Themepark {
 
-bool themepark_startup() {
+const f32 triangle[] = {
+  0.0f, 0.5f, 0.0f,
+  0.5f, -0.5f, 0.0f,
+  -0.5f, -0.5f, 0.0f,
+};
 
+const char* vertex_shader = 
+"#version 400\n"
+"in vec3 vp;\n"
+"void main() {\n"
+"   gl_Position = vec4(vp, 1.0);\n"
+"}\n";
+
+const char* fragment_shader = 
+"#version 400\n"
+"out vec4 frag_color;\n"
+"void main() {\n"
+"   frag_color = vec4(0.5, 0.0, 0.5, 1.0);\n"
+"}\n";
+
+
+u32 vao = 0;
+u32 shader_program = 0;
+
+
+bool themepark_startup() {
+  u32 vbo = 0;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), triangle, GL_STATIC_DRAW);
+
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+  glBindVertexArray(0);
+
+  u32 vs = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vs, 1, &vertex_shader, nullptr);
+  glCompileShader(vs);
+
+  u32 fs = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs, 1, &fragment_shader, nullptr);
+  glCompileShader(fs);
+
+  shader_program = glCreateProgram();
+  glAttachShader(shader_program, vs);
+  glAttachShader(shader_program, fs);
+  glLinkProgram(shader_program);
+
+  glClearColor(0.5f, 0.3f, 0.3f, 1.0f);
+
+  return true;
 }
 
 void themepark_run() {
-
-
+  glClear(GL_COLOR_BUFFER_BIT);
+  glUseProgram(shader_program);
+  glBindVertexArray(vao);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glBindVertexArray(0);
 }
 
 void themepark_shutdown() {
